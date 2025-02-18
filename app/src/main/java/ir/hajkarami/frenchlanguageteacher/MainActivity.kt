d5a9aa4661669aa44086f47adae554554468dcb7
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-18 10:13:02 +0330

    Initial commit

---
+++ app/src/main/java/ir/hajkarami/frenchlanguageteacher/MainActivity.kt
@@ -0,0 +1,22 @@
+package ir.hajkarami.frenchlanguageteacher
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ir.hajkarami.frenchlanguageteacher.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+    }
+}
