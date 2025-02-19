8ef28a6d3813a97d791a2e231e6b26b3ef70bee8
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-19 08:17:24 +0330

    Add audio files and fix MediaPlayer resource management Added necessary audio files (red_sound.mp3, black_sound.mp3, etc.). Implemented proper handling of MediaPlayer to play sounds correctly. Fixed memory leak by replacing setOnSeekCompleteListener with setOnCompletionListener to release resources after playback completion. Improved click listener setup for buttons using ViewBinding.

--- app/src/main/java/ir/hajkarami/frenchlanguageteacher/MainActivity.kt
+++ app/src/main/java/ir/hajkarami/frenchlanguageteacher/MainActivity.kt
@@ -1,13 +1,16 @@
 package ir.hajkarami.frenchlanguageteacher
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ir.hajkarami.frenchlanguageteacher.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +20,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        binding.btnRed.setOnClickListener ( this )
+        binding.btnBlack.setOnClickListener ( this )
+        binding.btnGreen.setOnClickListener ( this )
+        binding.btnYellow.setOnClickListener ( this )
+        binding.btnPurple.setOnClickListener ( this )
 
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            R.id.btn_red -> PlaySounds(R.raw.red)
+            R.id.btn_black -> PlaySounds(R.raw.black)
+            R.id.btn_green -> PlaySounds(R.raw.green)
+            R.id.btn_purple -> PlaySounds(R.raw.purple)
+            R.id.btn_yellow -> PlaySounds(R.raw.yellow)
+        }
+
+    }
+
+    private fun PlaySounds(Id: Int) {
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, Id)
+        mediaPlayer?.start()
+        mediaPlayer?.setOnCompletionListener { it.release() }
     }
 }
